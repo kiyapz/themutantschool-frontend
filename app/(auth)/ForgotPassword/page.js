f4361e5f5e567b940740b1d2895263fba31275fc
precious osonwa <osonwap083@gmail.com>
2025-06-20 04:08:38 +0100

    fixings bilud erros

--- app/(auth)/ForgotPassword/page.js
+++ app/(auth)/ForgotPassword/page.js
@@ -11,6 +11,7 @@ import { ForgotPasswordContext } from "./_components/ForgotpasswordContex";
 import OTPInput from "./_components/Otp";
 import authApiUrl from "@/lib/baseUrl";
 import { setErrorMap } from "zod";
+import Link from "next/link";
 
 export default function Forgotpassword() {
   const  {otpCode, setOtpCode} = useContext(  ForgotPasswordContext)
